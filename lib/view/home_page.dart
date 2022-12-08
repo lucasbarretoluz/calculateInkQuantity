d150ce325e8a6186c64498d000167ea3771e8cdf
DouglasPZR <douglas_paz_rodrigues@hotmail.com>
2022-12-07 22:00:16 -0300

    alteracoes de renderização do cubo e de giro Sliders X e Z com valores travados. Alterada a logica de renderização das paredes do cubo.

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -8,11 +9,12 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+double rx = 0.0, ry = 0.0, rz = 0.0;
+
 class _HomePageState extends State<HomePage> {
-  double rx = 0.0, ry = 0.0, rz = 0.0;
   @override
   Widget build(BuildContext context) {
-   print("RX: $rx, RY: $ry");
+    print("RX: $rx, RY: $ry, RZ: $rz");
     return GestureDetector(
       onPanUpdate: (details) {
         rx += details.delta.dx * 0.01;
@@ -34,7 +36,7 @@ class _HomePageState extends State<HomePage> {
                 transform: Matrix4.identity()
                   ..setEntry(3, 2, 0.001)
                   // ..rotateX(_offset.dy * pi / 180)
-                  // ..rotateY(_offset.dx * pi / 180),
+                  //..rotateY(_offset.dx * pi / 180),
                   ..rotateX(rx)
                   ..rotateY(ry)
                   ..rotateZ(rz),
@@ -46,7 +48,7 @@ class _HomePageState extends State<HomePage> {
               min: pi * -2,
               max: pi * 2,
               onChanged: ((value) => setState(() {
-                    rx = value;
+                    rx = -6.014827168191208;
                   })),
             ),
             Slider(
@@ -62,7 +64,7 @@ class _HomePageState extends State<HomePage> {
               min: pi * -2,
               max: pi * 2,
               onChanged: ((value) => setState(() {
-                    rz = value;
+                    rz = 6.283185307179586;
                   })),
             ),
           ],
@@ -77,75 +79,80 @@ class Cube extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Transform(
-          //Front
-          transform: Matrix4.identity()..translate(0.0, 0.0, -100.0),
-          alignment: Alignment.center,
-          child: Container(
-            color: Colors.red,
-            height: 200,
-            width: 200,
-          ),
-        ),
-        Transform(
-          //port
-          transform: Matrix4.identity()
-            ..translate(100.0, 0.0, 0.0)
-            ..rotateY(-pi / 2),
-          alignment: Alignment.center,
-          child: Container(
-            color: Colors.green,
-            height: 200,
-            width: 200,
-          ),
-        ),
-        Transform(
-          transform: Matrix4.identity()
-            ..translate(0.0, 100.0, 0.0)
-            ..rotateX(pi / 2),
-          alignment: Alignment.center,
-          child: Container(
-            color: Colors.blue,
-            height: 200,
-            width: 200,
-          ),
-        ),
-        Transform(
-          //back
-          transform: Matrix4.identity()..translate(0.0, 0.0, 100.0),
-          alignment: Alignment.center,
-          child: Container(
-            color: Colors.purple,
-            height: 200,
-            width: 200,
-          ),
-        ),
-        Transform(
-          //port
-          transform: Matrix4.identity()
-            ..translate(-100.0, 0.0, 0.0)
-            ..rotateY(-pi / 2),
-          alignment: Alignment.center,
-          child: Container(
-            color: Colors.yellow,
-            height: 200,
-            width: 200,
-          ),
-        ),
-        Transform(
-          transform: Matrix4.identity()
-            ..translate(0.0, -100.0, 0.0)
-            ..rotateX(-pi / 2),
-          alignment: Alignment.center,
-          child: Container(
-            color: Colors.amber,
-            height: 200,
-            width: 200,
-          ),
-        ),
-      ],
+    final List<Widget> childrens;
+    late final sideA = Transform(
+      //Front
+      transform: Matrix4.identity()
+        ..translate(0.0, 0.0, -100.0)
+        ..rotateY(-pi),
+      alignment: Alignment.center,
+      child: Container(
+        color: Colors.red,
+        height: 200,
+        width: 200,
+      ),
     );
+
+    late final sideB = Transform(
+      //port
+      transform: Matrix4.identity()
+        ..translate(100.0, 0.0, 0.0)
+        ..rotateY(pi / 2),
+      alignment: Alignment.center,
+      child: Container(
+        color: Colors.green,
+        height: 200,
+        width: 200,
+      ),
+    );
+
+    late final sideC = Transform(
+      //back
+      transform: Matrix4.identity()..translate(0.0, 0.0, 100.0),
+      alignment: Alignment.center,
+      child: Container(
+        color: Colors.purple,
+        height: 200,
+        width: 200,
+      ),
+    );
+
+    late final sideD = Transform(
+      //port
+      transform: Matrix4.identity()
+        ..translate(-100.0, 0.0, 0.0)
+        ..rotateY(-pi / 2),
+      alignment: Alignment.center,
+      child: Container(
+        color: Colors.yellow,
+        height: 200,
+        width: 200,
+      ),
+    );
+
+    late final topSide = Transform(
+      transform: Matrix4.identity()
+        ..translate(0.0, -100.0, 0.0)
+        ..rotateX(-pi / 2),
+      alignment: Alignment.center,
+      child: Container(
+        color: Colors.black,
+        height: 200,
+        width: 200,
+      ),
+    );
+
+    if (ry < -pi / 4) {
+      childrens = [sideA, sideB];
+    } else if (ry < -pi / 2) {
+      childrens = [sideB, sideC, topSide];
+    } else if (ry < 3 * pi / 4) {
+      childrens = [sideC, sideD, topSide];
+    } else {
+      childrens = [sideD, sideA, topSide];
+    }
+
+    childrens.add(topSide);
+    return Stack(children: childrens);
   }
 }
